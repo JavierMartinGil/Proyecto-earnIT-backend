4fc12a7f5420e26227c4d7a24d05ed4ba59da8dd
javier_martin83 <javier_martin83@yahoo.es>
2019-09-05 15:06:14 +0200

    Peticion a la bbdd sobre el sueldo medio OK

--- models/compara.js
+++ models/compara.js
@@ -2,12 +2,14 @@ const db = require('../db');
 
 const consult = ({ cargo, experiencia, tecnologia }) => {
     return new Promise((resolve, reject) => {
-        let q = 'select AVG(bruto) from tabla_sueldos where cargo= ? and experiencia=? and tecnologia=?;';
-        db.get().query(q, [cargo, experiencia, tecnologia], (err, rows) => {
+        let q = "select AVG(bruto) AS salario_medio from tabla_sueldos where cargo= ? and experiencia=? and tecnologia=?; select * from tabla_sueldos where cargo = ? and experiencia = ? and tecnologia = ?"
+        /*select AVG(bruto) AS salario_medio from tabla_sueldos where cargo = "Front-end" and experiencia = 3 and tecnologia = "Javascript";
+          select * from tabla_sueldos where cargo = "Front-end" and experiencia = 3 and tecnologia = "Javascript"*/
+        db.get().query(q, [cargo, experiencia, tecnologia, cargo, experiencia, tecnologia], (err, rows) => {
             if (err) reject(err)
             resolve(rows)
         });
-    })
+    });
 }
 
 module.exports = {
--- routes/compara.js
+++ routes/compara.js
@@ -6,12 +6,10 @@ const modelCompara = require('../models/compara');
 // http://localhost:3000/compara/salarios
 
 router.post('/salarios', (req, res) => {
-    console.log(req.body);
+
     modelCompara.consult(req.body)
         .then((result) => {
-            console.log(result);
             res.json(result);
-
         })
         .catch((err) => {
             res.json({ error: err });

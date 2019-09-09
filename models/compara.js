4d37133dd093b60d849f1122921c3f0c19ffba35
Javier Martin Gil <javiermartingil83@gmail.com>
2019-09-09 16:00:40 +0200

    Limitada la query sql

--- models/compara.js
+++ models/compara.js
@@ -2,7 +2,7 @@ const db = require('../db');
 
 const consult = ({ cargo, experiencia, tecnologia }) => {
     return new Promise((resolve, reject) => {
-        let q = "select AVG(bruto) AS salario_medio from tabla_sueldos where cargo= ? and experiencia=? and tecnologia=?; select * from tabla_sueldos where cargo = ? and experiencia = ? and tecnologia = ?"
+        let q = "select AVG(bruto) AS salario_medio from tabla_sueldos where cargo= ? and experiencia=? and tecnologia=?; select * from  tabla_sueldos  where cargo = ? and experiencia = ? and tecnologia = ? limit 5"
         /*select AVG(bruto) AS salario_medio from tabla_sueldos where cargo = "Front-end" and experiencia = 3 and tecnologia = "Javascript";
           select * from tabla_sueldos where cargo = "Front-end" and experiencia = 3 and tecnologia = "Javascript"*/
         db.get().query(q, [cargo, experiencia, tecnologia, cargo, experiencia, tecnologia], (err, rows) => {

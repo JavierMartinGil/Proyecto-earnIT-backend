16540881862a6df419727a68c947d2acceb1fe81
Javier Martin Gil <javiermartingil83@gmail.com>
2019-09-10 17:41:42 +0200

    Borrar comentario

--- models/compara.js
+++ models/compara.js
@@ -3,8 +3,6 @@ const db = require('../db');
 const consult = ({ cargo, experiencia, tecnologia }) => {
     return new Promise((resolve, reject) => {
         let q = "select AVG(bruto) AS salario_medio from tabla_sueldos where cargo= ? and experiencia=? and tecnologia=?; select * from  tabla_sueldos  where cargo = ? and experiencia = ? and tecnologia = ? limit 5"
-        /*select AVG(bruto) AS salario_medio from tabla_sueldos where cargo = "Front-end" and experiencia = 3 and tecnologia = "Javascript";
-          select * from tabla_sueldos where cargo = "Front-end" and experiencia = 3 and tecnologia = "Javascript"*/
         db.get().query(q, [cargo, experiencia, tecnologia, cargo, experiencia, tecnologia], (err, rows) => {
             if (err) reject(err)
             resolve(rows)
